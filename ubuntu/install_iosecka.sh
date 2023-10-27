da541174486ef3bd9d40b428bfd9d424e4a37399
huuk2077 <wk.huang@outlook.com>
2023-10-27 03:13:39 +0000

    fix: install iosecka font

--- ubuntu/install_iosecka.sh
+++ ubuntu/install_iosecka.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 RELEASE="27.3.1"
-mkdir /tmp/iosevka-font/$RELEASE
-cd /tmp/iosevka-font/$RELEASE
-wget https://github.com/be5invis/Iosevka/releases/download/v$RELEASE/super-ttc-iosevka-$RELEASE.zip
+download_dir="/tmp/iosevka-font"
+base_url="https://github.com/be5invis/Iosevka/releases/download/v$RELEASE"
+font_dir="$HOME/.local/share/fonts/"
+
+mkdir -p "$download_dir" || { echo "无法创建下载目录"; exit 1; }
+cd "$download_dir" || { echo "无法进入下载目录"; exit 1; }
+wget $base_url/super-ttc-iosevka-$RELEASE.zip || { echo "无法下载字体文件"; exit 1; }
 unzip \*.zip
-sudo cp *.ttc /usr/local/share/fonts/
-cd /usr/local/share/fonts/
-sudo mkfontscale
-sudo mkfontdir
-sudo fc-cache
+
+mkdir -p "$font_dir" || { echo "无法创建字体目录"; exit 1; }
+mv *.ttc "$font_dir" || { echo "无法移动字体文件"; exit 1; }
+
+fc-cache -f -v || { echo "无法更新字体缓存"; exit 1; }
+
 cd -
-fc-list
+rm -r "$download_dir" || { echo "无法清理下载目录"; exit 1; }
+
+echo "Done"
