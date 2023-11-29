e3cf7c5553538655c24bc0bd36096eeeb3dda099
hwk2077 <wk.huang2077@outlook.com>
2023-11-29 15:28:29 +0800

    add zsh plugins

--- ohmyzsh/install_ohmyzsh.sh
+++ ohmyzsh/install_ohmyzsh.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# plugin, need to change .zshrc file.
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
